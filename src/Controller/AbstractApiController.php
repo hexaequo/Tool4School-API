9ecdf2c9a0e6e001ba278b05a3e0d0099a547385
Mike Devresse <mike.devresse@gmail.com>
2021-02-17 21:36:21 +0100

    Added a dispatch method that save the message in cache

--- src/Controller/AbstractApiController.php
+++ src/Controller/AbstractApiController.php
@@ -6,12 +6,19 @@ namespace App\Controller;
 
 use App\Exception\Request\JsonNotParsableException;
 use App\Exception\Request\WrongContentTypeException;
+use App\Messenger\ArrayMessage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Contracts\Cache\CacheInterface;
 
 abstract class AbstractApiController extends AbstractController
 {
+    public function __construct(private CacheInterface $cache)
+    {
+    }
+
     public function checkContentType(Request $request, $contentType = 'application/json') {
         if($request->headers->get('Content-Type') != $contentType) {
             throw new WrongContentTypeException($contentType);
@@ -42,5 +49,16 @@ abstract class AbstractApiController extends AbstractController
             'id' => $id,
             'href' => '/job/'.$id
         ],202);
+    }
+
+    public function dispatchCachedMessage(ArrayMessage $message, array $stamps = []): Envelope
+    {
+        $this->cache->delete($message->getId());
+
+        $this->cache->get($message->getId(),function() use ($message) {
+            return $message;
+        });
+
+        return parent::dispatchMessage($message, $stamps);
     }
 }
