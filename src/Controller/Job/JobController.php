91993132ee6278fec7ffba83a28aaa4e8296f926
Mike Devresse <mike.devresse@gmail.com>
2021-02-17 21:36:58 +0100

    Edited jobController to return times of response and return 404 if job id is not known

--- src/Controller/Job/JobController.php
+++ src/Controller/Job/JobController.php
@@ -10,36 +10,47 @@ use App\Messenger\ArrayMessage;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class JobController extends AbstractApiController
 {
     #[Route("/job/{id}", methods: ['GET'])]
-    public function getJob(string $id, CacheInterface $cache) {
+    public function getJob(string $id, CacheInterface $cache, SerializerInterface $serializer) {
         $jobResponse = $cache->get($id,function() { return null; });
         if($jobResponse) {
             if($jobResponse instanceof ArrayMessage){
-                $statusCode = $jobResponse->getData()['code'] ?? 200;
+                $headers = ['Content-Type'=>'application/json'];
+                $statusCode = 200;
+                if($jobResponse->isEnded()) {
+                    $data = $jobResponse->getData();
+                    if(isset($data['code'])) {
+                        $statusCode = $data['code'];
+                        unset($data['code']);
+                    }
 
-                if(substr($statusCode,0,1) !== '2' and isset($jobResponse->getData()['error'])) {
-                    throw new ApiException($statusCode,json_encode($jobResponse->getData()['error']));
+                    if(isset($data['Content-Location'])) {
+                        $headers['Content-Location'] = $data['Content-Location'];
+                        unset($data['Content-Location']);
+                    }
+
+                    $jobResponse->setData($data);
+                }
+                else {
+                    $statusCode = 202;
+                    $jobResponse->setData([]);
                 }
 
-                if(isset($jobResponse->getData()['Content-Location'])) {
-                    return new Response(null,$statusCode,[
-                        'Content-Location' => $jobResponse->getData()['Content-Location']
-                    ]);
-                }
-
-                $data = $jobResponse->getData();
-                if(isset($data['code'])) unset($data['code']);
-
-                return new JsonResponse($data,$statusCode);
+                return new Response(
+                    $serializer->serialize($jobResponse,'json'),
+                    $statusCode,
+                    $headers
+                );
             }
             else {
                 return new JsonResponse(['error'=>'Unknown'],500);
             }
         }
-        return new Response(null,202);
+        return new Response(null,404);
     }
 }
